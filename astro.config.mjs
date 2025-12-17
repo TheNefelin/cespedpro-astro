a17db9ae2fbb68325378b7da1848b3953c7b02de
Francisco Carmona <flcarmonac@yahoo.com>
2025-12-17 19:59:43 -0300

    Update: try to deploy on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://thenefelin.github.io',
-  base: '/cespedpro-astro',
   vite: {
     plugins: [
       tailwindcss()
